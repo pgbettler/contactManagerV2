81131e8cab90478f80a5ed0582320304b527cd52
Lauren Lederer <lederer.lauren@gmail.com>
2019-06-05 22:40:38 -0400

    Homepage background added

--- frontend/my-vueapp/src/App.vue
+++ frontend/my-vueapp/src/App.vue
@@ -15,7 +15,6 @@
 
 <script>
 import HomeComponent from './components/HomeComponent.vue';
-
 export default {
   name: 'app',
   components: {
@@ -25,19 +24,24 @@ export default {
 </script>
 
 <style>
+body {
+    background-image: url("../src/assets/underwater.jpg");
+    background-repeat: no-repeat;
+    background-size: 100%;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
 }
     .topnav {
         overflow: hidden;
         background-image: linear-gradient(#55C2D7,#317B94) ;
         box-shadow: 0 4px 8px 0 #07889B, 0 6px 20px 0 rgba(0, 0, 0, 0.19);
     }
-
     .logoimage {
     padding: 0px 300px 500px 0px;
     }
@@ -45,7 +49,6 @@ export default {
     transition: .5s;
     box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
     }
-
     /* Style the links inside the navigation bar */
     .topnav h1 {
     float: left;
@@ -58,17 +61,14 @@ export default {
         color: white;
         text-decoration: none;
     }
-
     /* Add a color to the active/current link */
     .topnav a.active {
     color: white;
     }
-
     /* Right-aligned section inside the top navigation */
     .topnav-right {
     float: right;
     padding: 30px 80px;
-
     }
     .topnav-right a {
         font-size: 30px;
